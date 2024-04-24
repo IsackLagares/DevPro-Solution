f1ae8e6e54e4354a35087f68f37408395882a2ec
IsackLagares <isackrafael10@gmail.com>
2024-04-24 13:50:37 -0300

    ajustes

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -1,24 +1,18 @@
 const modal = document.getElementById('modal'); 
 
 function openModal() {
-    modal.classList.add('view');
-
-    function closeModal() {
-        modal.classList.add('closeModal');
-        setTimeout(function() {
-            modal.classList.remove('view', 'closeModal');
-        }, 500);
-    }
-
-    modal.addEventListener('click', (e) => {
-        if (!modal.contains(e.target)) {
-            closeModal();
-        }
-    });
-
-    // Remova a classe 'closeModal' quando a animação de fechamento for concluída
-    modal.addEventListener('animationend', () => {
-        modal.classList.remove('closeModal');
-    });
+    modal.style.display = 'block';
+    modal.style.animation = 'openModal 0.5s';
 }
+
+window.onload = function () {
+    document.onclick = function(element) {
+        if(element.target.id !== 'modal' && element.target.id !== 'btn') {
+            modal.style.animation = 'closeModal 0.5s';
+            modal.style.display = 'none';
+        }
+    };
+};
+
+
 
