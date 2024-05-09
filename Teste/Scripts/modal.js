1d7df1fdee28b4383e91aabe1cea8041d6087da9
gabrieldemetrio571@gmail.com <gabrieldemetrio571@gmail.com>
2024-05-08 22:06:30 -0300

    card mostrando contato

--- Teste/Scripts/modal.js
+++ Teste/Scripts/modal.js
@@ -1,4 +1,4 @@
-const modal = document.getElementById('modal'); 
+const modal = document.getElementById('modal');
 
 function openModal() {
     modal.style.display = 'block';
@@ -6,11 +6,48 @@ function openModal() {
 }
 
 window.onload = function () {
-    document.onclick = function(element) {
-        if(element.target.id !== 'modal' && element.target.id !== 'send' && element.target.id !== 'home-btn' && element.target.id !== 'input-box') {
+    document.onclick = function (element) {
+        if (element.target.id !== 'modal' && element.target.id !== 'send' && element.target.id !== 'home-btn' && element.target.id !== 'input-box') {
             modal.style.animation = 'closeModal 0.5s';
             modal.style.display = 'none';
         }
     };
 }
 
+const contact = document.getElementById('contact');
+const card = document.getElementById('card');
+
+function openCard() {
+    contact.addEventListener('mouseover', () => {
+        card.style.display = 'block';
+        card.style.animation = 'openCard 0.5s';
+    });
+}
+
+// function showCard() {
+//     card.addEventListener('mouseover', () => {
+//         card.style.display = 'block';
+//         card.style.animation = 'openCard 0.5s';
+//     });
+// }
+
+function showCard() {
+    if (card.classList.contains('closed')) {
+        card.classList.remove('closed');
+    } else {
+        card.classList.add('closed');
+    }
+}
+
+function hideCard() {
+    contact.addEventListener('mouseout', () => {
+        card.style.display = 'none';
+    });
+}
+
+function closeCard() {
+    contact.addEventListener('mouseout', () => {
+        card.style.display = 'contents';
+        card.style.animation = 'closeCard 1.0s';
+    });
+}
