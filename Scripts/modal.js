886d4467494be45fa5b110f19c978d128f0aa7a1
IsackLagares <isackrafael10@gmail.com>
2024-06-19 10:17:59 -0300

    atualizacao repositorio

--- Scripts/modal.js
+++ Scripts/modal.js
@@ -1,4 +1,5 @@
 const modal = document.getElementById('modal');
+const tmodal = document.getElementById('tmodal');
 
 function openModal() {
     modal.style.display = 'block';
@@ -12,4 +13,52 @@ window.onload = function () {
             modal.style.display = 'none';
         }
     };
+}
+
+function openTModal() {
+    modal.style.animation = 'closeModal 0.5s';
+    modal.style.display = 'none';
+    setTimeout(() => {
+        tmodal.style.animation = 'openModal 0.5s';
+    }, 500);
+}
+
+const contact = document.getElementById('contact');
+const card = document.getElementById('card');
+
+function openCard() {
+    contact.addEventListener('mouseover', () => {
+        card.style.display = 'block';
+        card.style.animation = 'openCard 0.5s';
+    });
+}
+
+// function showCard() {
+//     card.addEventListener('mouseover', () => {
+//         card.style.display = 'block';
+//         card.style.animation = 'openCard 0.5s';
+//     });
+// }
+
+function showCard() {
+    if (card.classList.contains('closed')) {
+        card.classList.remove('closed');
+    } else {
+        card.classList.add('closed');
+    }
+}
+
+function hideCard() {
+    contact.addEventListener('mouseout', () => {
+        card.style.display = 'none';
+    });
+}
+
+function closeCard() {
+    contact.addEventListener('mouseout', () => {
+        card.style.display = 'contents';
+        setTimeout(() => {
+            card.style.animation = 'closeCard 5.0s';
+        }, 5000);
+    });
 }
--- Teste/Scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('.menu');
-const NavMenu = doument.querySelector('nav-menu');
-
-menu.addEventListener('click', () => {
-    menu.classList.toggle('ativo');
-    NavMenu.classList.toggle('ativo');
-})
